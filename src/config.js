068ebd8b8a108ab33a400b398f28aa71ba49f186
westinaa <162748407+westinaa@users.noreply.github.com>
2025-04-16 00:30:05 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    presence: {
+        name: ['/izlerkalirsin', "izlerkalırsın", "izlerkalirsin", "@izlerkalırsın", "@izlerkalirsin", ".gg/izlerkalirsin", ".gg/izlerkalırsın"],
+    },
+    guild: {
+        roleId: '1357161320679477459',
+        channelId: '1361694302811586560',
+        guildId: '1357115287044100216',
+    },
+    discord: {
+        token: process.env.TOKEN,
+        id: '796677658745765898',
+    },
+    owners: [
+        '474006896408264712',
+    ],
+};
