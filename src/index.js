280fc29b8d493d550d40131e99b113cdad71e51e
westinaa <162748407+westinaa@users.noreply.github.com>
2025-04-16 00:27:52 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const { Client, IntentsBitField, Partials } = require('discord.js');
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+const config = require('./config.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildPresences,
+        IntentsBitField.Flags.GuildVoiceStates,
+    ],
+    partials: [
+        Partials.User,
+    ],
+});
+
+const listenersPath = path.join(__dirname, './listeners');
+const listenersFiles = fs.readdirSync(listenersPath).filter(file => file.endsWith('.js'));
+
+for (const file of listenersFiles) {
+    const filePath = path.join(listenersPath, file);
+    /**
+     * @interface @type {{ name: keyof import('discord.js').ClientEvents; once: boolean; run: (...args: any) => void | Promise<void> }}
+     */
+    const listener = require(filePath);
+
+    if ('name' in listener && 'run' in listener) {
+        client[ listener.once ? 'once' : 'on' ](listener.name, (...args) => { listener.run(...args); });
+    }
+}
+
+client.once("ready", () => {
+
+console.log(`Online: ${client.user.tag}`);
+
+  client.user.setPresence({
+    activities: [{ name: "@izlerkalirsin", type: "LISTENING" }],
+    status: 'online',
+  });
+    
+     // Ses kanalına bağlanma
+    const botVoiceChannel = client.channels.cache.get("1357154558870163647");
+    if (!botVoiceChannel) {
+      return console.error("[HATA] Ses kanalı bulunamadı!");
+    }
+  
+    try {
+      const connection = joinVoiceChannel({
+        channelId: botVoiceChannel.id,
+        guildId: botVoiceChannel.guild.id,
+        adapterCreator: botVoiceChannel.guild.voiceAdapterCreator,
+      });
+  
+      console.log("Bot ses kanalına bağlandı!");
+    } catch (err) {
+      console.error("[HATA] Bot ses kanalına bağlanamadı!", err);
+    }
+  });
+
+client.login(config.discord.token)
+    .then(() => {
+        console.log('[LOGIN] Discord API\'ye istek gönderimi başarılı.');
+    })
+    .catch(() => {
+        console.log('[LOGIN] Discord API\'ye istek gönderimi başarısız.');
+    });
