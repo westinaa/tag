546e84349f56d1c8be20d2922838a5c94f656c50
westinaa <162748407+westinaa@users.noreply.github.com>
2025-04-16 00:52:54 +0300

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -31,82 +31,84 @@ module.exports = {
                 const [ first ] = presence?.activities || [];
                 const text = first?.type === ActivityType.Custom ? first?.state : first?.name;
 
-                if (hasRole && text !== name) {
-                    member.roles.remove(role, 'discord.gg/izlerkalirsin')
-                    .then(() => {
-                        channel.send({
-                            embeds: [
-                                new EmbedBuilder()
-                                .setColor("Red")
-                                .setTitle('<a:nebakiypn:1361434527448432791> Bir kişi aramızdan ayrıldı. <:emoji_9:1358335169966112768>')
-                                .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })    
-                                .setDescription(`• <@&1357161320679477459> rolü, ${member.displayName}'nın/nin durum mesajından \`${name}\` ifadesini kaldırdığı için kendisinden **alındı**.`)
-                                .setThumbnail(member.user.displayAvatarURL())
-                                .setFields([
-                                    {
-                                        name: 'Kullanıcı etiketi:',
-                                        value: `> <@${member.user.id}>`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: 'Güncelleme saati:',
-                                        value: `> ${time(parseInt(`${Date.now() / 1000}`), 'R')}`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: 'Toplam kişi:',
-                                        value: `> ${index}.`,
-                                        inline: true,
-                                    },
-                                ])
-                                .setTimestamp()
-                                .setFooter({ text: 'discord.gg/izlerkalirsin', iconURL: "https://images-ext-1.discordapp.net/external/xvbBiGKwN3qcDFcOIVeAy3LTBzYf4pUz2RZDA3N0sRw/https/cdn.discordapp.com/icons/1357115287044100216/00e1e2b184a2b0ebef7b861aa62438a4.webp?format=webp"}),
-                            ],
-                          });
-                        })
-                        .catch(() => undefined);
+                // "name" dizisindeki her elemanla kontrol yapıyoruz
+                for (let presenceText of name) {
+                    if (hasRole && text !== presenceText) {
+                        member.roles.remove(role, 'discord.gg/izlerkalirsin')
+                            .then(() => {
+                                channel.send({
+                                    embeds: [
+                                        new EmbedBuilder()
+                                        .setColor("Red")
+                                        .setTitle('<a:nebakiypn:1361434527448432791> Bir kişi aramızdan ayrıldı. <:emoji_9:1358335169966112768>')
+                                        .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })    
+                                        .setDescription(`• <@&1357161320679477459> rolü, ${member.displayName}'nın/nin durum mesajından \`${presenceText}\` ifadesini kaldırdığı için kendisinden **alındı**.`)
+                                        .setThumbnail(member.user.displayAvatarURL())
+                                        .setFields([
+                                            {
+                                                name: 'Kullanıcı etiketi:',
+                                                value: `> <@${member.user.id}>`,
+                                                inline: true,
+                                            },
+                                            {
+                                                name: 'Güncelleme saati:',
+                                                value: `> ${time(parseInt(`${Date.now() / 1000}`), 'R')}`,
+                                                inline: true,
+                                            },
+                                            {
+                                                name: 'Toplam kişi:',
+                                                value: `> ${index}.`,
+                                                inline: true,
+                                            },
+                                        ])
+                                        .setTimestamp()
+                                        .setFooter({ text: 'discord.gg/izlerkalirsin', iconURL: "https://images-ext-1.discordapp.net/external/xvbBiGKwN3qcDFcOIVeAy3LTBzYf4pUz2RZDA3N0sRw/https/cdn.discordapp.com/icons/1357115287044100216/00e1e2b184a2b0ebef7b861aa62438a4.webp?format=webp"}),
+                                    ],
+                                });
+                            })
+                            .catch(() => undefined);
                     
-                    continue;
-                }
+                        continue;
+                    }
 
-                if (hasRole) continue;
-                if (text !== name) continue;
+                    if (hasRole) continue;
+                    if (text !== presenceText) continue;
 
-                member.roles.add(role, 'discord.gg/izlerkalirsin')
-                    .then(() => {
-                        channel.send({
-                            embeds: [
-                                new EmbedBuilder()
-                                .setColor("Green")
-                                .setTitle('<:emoji_10:1358335186776887447> Bir kişi daha aramıza katıldı! <:of:1361434200930124057>')
-                                .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })    
-                                .setDescription(`• <@&1357161320679477459> rolü, ${member.displayName}'nın/nin durum mesajını \`${name}\` yapması nedeniyle kendisine **verildi**.`)
-                                .setThumbnail(member.user.displayAvatarURL())
-                                .setFields([
-                                    {
-                                        name: 'Kullanıcı etiketi:',
-                                        value: `> <@${member.user.id}>`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: 'Güncelleme saati:',
-                                        value: `> ${time(parseInt(`${Date.now() / 1000}`), 'R')}`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: 'Toplam kişi:',
-                                        value: `> ${index}.`,
-                                        inline: true,
-                                    },
-                                ])
-                                .setTimestamp()
-                                .setFooter({ text: 'discord.gg/izlerkalirsin', iconURL: 'https://images-ext-1.discordapp.net/external/xvbBiGKwN3qcDFcOIVeAy3LTBzYf4pUz2RZDA3N0sRw/https/cdn.discordapp.com/icons/1357115287044100216/00e1e2b184a2b0ebef7b861aa62438a4.webp?format=png'}),
-                            ],
+                    member.roles.add(role, 'discord.gg/izlerkalirsin')
+                        .then(() => {
+                            channel.send({
+                                embeds: [
+                                    new EmbedBuilder()
+                                    .setColor("Green")
+                                    .setTitle('<:emoji_10:1358335186776887447> Bir kişi daha aramıza katıldı! <:of:1361434200930124057>')
+                                    .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })    
+                                    .setDescription(`• <@&1357161320679477459> rolü, ${member.displayName}'nın/nin durum mesajını \`${presenceText}\` yapması nedeniyle kendisine **verildi**.`)
+                                    .setThumbnail(member.user.displayAvatarURL())
+                                    .setFields([
+                                        {
+                                            name: 'Kullanıcı etiketi:',
+                                            value: `> <@${member.user.id}>`,
+                                            inline: true,
+                                        },
+                                        {
+                                            name: 'Güncelleme saati:',
+                                            value: `> ${time(parseInt(`${Date.now() / 1000}`), 'R')}`,
+                                            inline: true,
+                                        },
+                                        {
+                                            name: 'Toplam kişi:',
+                                            value: `> ${index}.`,
+                                            inline: true,
+                                        },
+                                    ])
+                                    .setTimestamp()
+                                    .setFooter({ text: 'discord.gg/izlerkalirsin', iconURL: 'https://images-ext-1.discordapp.net/external/xvbBiGKwN3qcDFcOIVeAy3LTBzYf4pUz2RZDA3N0sRw/https/cdn.discordapp.com/icons/1357115287044100216/00e1e2b184a2b0ebef7b861aa62438a4.webp?format=png'}),
+                                ],
+                            })
+                            .catch(() => undefined);
                         })
                         .catch(() => undefined);
-                    })
-                    .catch(() => undefined);
-              
+                }
             }
         }
     },
